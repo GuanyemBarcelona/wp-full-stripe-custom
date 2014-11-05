dbe09e8ca39b6a7a5e0650540e39d45aa584b992
Felip Ladron <fladron@gmail.com>
2014-11-05 01:09:11 +0100

    Developing the countries and states selectors

--- js/wp-full-stripe.js
+++ js/wp-full-stripe.js
@@ -299,7 +299,7 @@ jQuery(document).ready(function ($)
         return false;
     });
 
-    var _getSpanishProvinces = function (){
+    var _getSpanishProvinces = function(){
       return [
               "Álava/Araba",
               "Albacete",
@@ -355,24 +355,67 @@ jQuery(document).ready(function ($)
               "Zaragoza"
           ];
     };
-    // Province select list: This is only valid for Spain
-    var province_input = $('#fullstripe_address_state');
-    if (province_input.length){
-      var provinces = _getSpanishProvinces();
-      for (var i in provinces){
-        var province_name = provinces[i];
-        var selected = '';
-        if (province_name == 'Barcelona') selected = ' selected="selected"';
-        province_input.append('<option value="'+province_name+'"'+selected+'>'+province_name+'</option>');
+
+    // Spanish Provinces select list
+    var fillRegionSelectWithSpanishProvinces = function(){
+      var input = $('[name="fullstripe_address_state"]');
+      if (input.length){
+        if (input.is('input')) input.replaceWith('<select name="fullstripe_address_state" id="fullstripe_address_state"></select>');
+        var provinces = _getSpanishProvinces();
+        for (var i in provinces){
+          var province_name = provinces[i];
+          var selected = '';
+          if (province_name == 'Barcelona') selected = ' selected="selected"';
+          console.log(input);
+          input.append('<option value="'+province_name+'"'+selected+'>'+province_name+'</option>');
+        }
       }
-    }
+    };
+
+    var changeRegionSelectToInput = function(){
+      var input = $('[name="fullstripe_address_state"]');
+      if (input.length){
+        if (input.is('select')) input.replaceWith('<input type="text" name="fullstripe_address_state" id="fullstripe_address_state">');
+      }
+    };
+
+    var _getWorldCountries = function(){
+      return {"AF":"Afghanistan","AL":"Albania","DZ":"Algeria","AS":"American Samoa","AD":"Andorra","AO":"Angola","AI":"Anguilla","AQ":"Antarctica","AG":"Antigua and Barbuda","AR":"Argentina","AM":"Armenia","AW":"Aruba","AU":"Australia","AT":"Austria","AZ":"Azerbaijan","BS":"Bahamas","BH":"Bahrain","BD":"Bangladesh","BB":"Barbados","BY":"Belarus","BE":"Belgium","BZ":"Belize","BJ":"Benin","BM":"Bermuda","BT":"Bhutan","BO":"Bolivia","BA":"Bosnia and Herzegovina","BW":"Botswana","BV":"Bouvet Island","BR":"Brazil","BQ":"British Antarctic Territory","IO":"British Indian Ocean Territory","VG":"British Virgin Islands","BN":"Brunei","BG":"Bulgaria","BF":"Burkina Faso","BI":"Burundi","KH":"Cambodia","CM":"Cameroon","CA":"Canada","CT":"Canton and Enderbury Islands","CV":"Cape Verde","KY":"Cayman Islands","CF":"Central African Republic","TD":"Chad","CL":"Chile","CN":"China","CX":"Christmas Island","CC":"Cocos [Keeling] Islands","CO":"Colombia","KM":"Comoros","CG":"Congo - Brazzaville","CD":"Congo - Kinshasa","CK":"Cook Islands","CR":"Costa Rica","HR":"Croatia","CU":"Cuba","CY":"Cyprus","CZ":"Czech Republic","CI":"Côte d’Ivoire","DK":"Denmark","DJ":"Djibouti","DM":"Dominica","DO":"Dominican Republic","NQ":"Dronning Maud Land","DD":"East Germany","EC":"Ecuador","EG":"Egypt","SV":"El Salvador","GQ":"Equatorial Guinea","ER":"Eritrea","EE":"Estonia","ET":"Ethiopia","FK":"Falkland Islands","FO":"Faroe Islands","FJ":"Fiji","FI":"Finland","FR":"France","GF":"French Guiana","PF":"French Polynesia","TF":"French Southern Territories","FQ":"French Southern and Antarctic Territories","GA":"Gabon","GM":"Gambia","GE":"Georgia","DE":"Germany","GH":"Ghana","GI":"Gibraltar","GR":"Greece","GL":"Greenland","GD":"Grenada","GP":"Guadeloupe","GU":"Guam","GT":"Guatemala","GG":"Guernsey","GN":"Guinea","GW":"Guinea-Bissau","GY":"Guyana","HT":"Haiti","HM":"Heard Island and McDonald Islands","HN":"Honduras","HK":"Hong Kong SAR China","HU":"Hungary","IS":"Iceland","IN":"India","ID":"Indonesia","IR":"Iran","IQ":"Iraq","IE":"Ireland","IM":"Isle of Man","IL":"Israel","IT":"Italy","JM":"Jamaica","JP":"Japan","JE":"Jersey","JT":"Johnston Island","JO":"Jordan","KZ":"Kazakhstan","KE":"Kenya","KI":"Kiribati","KW":"Kuwait","KG":"Kyrgyzstan","LA":"Laos","LV":"Latvia","LB":"Lebanon","LS":"Lesotho","LR":"Liberia","LY":"Libya","LI":"Liechtenstein","LT":"Lithuania","LU":"Luxembourg","MO":"Macau SAR China","MK":"Macedonia","MG":"Madagascar","MW":"Malawi","MY":"Malaysia","MV":"Maldives","ML":"Mali","MT":"Malta","MH":"Marshall Islands","MQ":"Martinique","MR":"Mauritania","MU":"Mauritius","YT":"Mayotte","FX":"Metropolitan France","MX":"Mexico","FM":"Micronesia","MI":"Midway Islands","MD":"Moldova","MC":"Monaco","MN":"Mongolia","ME":"Montenegro","MS":"Montserrat","MA":"Morocco","MZ":"Mozambique","MM":"Myanmar [Burma]","NA":"Namibia","NR":"Nauru","NP":"Nepal","NL":"Netherlands","AN":"Netherlands Antilles","NT":"Neutral Zone","NC":"New Caledonia","NZ":"New Zealand","NI":"Nicaragua","NE":"Niger","NG":"Nigeria","NU":"Niue","NF":"Norfolk Island","KP":"North Korea","VD":"North Vietnam","MP":"Northern Mariana Islands","NO":"Norway","OM":"Oman","PC":"Pacific Islands Trust Territory","PK":"Pakistan","PW":"Palau","PS":"Palestinian Territories","PA":"Panama","PZ":"Panama Canal Zone","PG":"Papua New Guinea","PY":"Paraguay","YD":"People's Democratic Republic of Yemen","PE":"Peru","PH":"Philippines","PN":"Pitcairn Islands","PL":"Poland","PT":"Portugal","PR":"Puerto Rico","QA":"Qatar","RO":"Romania","RU":"Russia","RW":"Rwanda","RE":"Réunion","BL":"Saint Barthélemy","SH":"Saint Helena","KN":"Saint Kitts and Nevis","LC":"Saint Lucia","MF":"Saint Martin","PM":"Saint Pierre and Miquelon","VC":"Saint Vincent and the Grenadines","WS":"Samoa","SM":"San Marino","SA":"Saudi Arabia","SN":"Senegal","RS":"Serbia","CS":"Serbia and Montenegro","SC":"Seychelles","SL":"Sierra Leone","SG":"Singapore","SK":"Slovakia","SI":"Slovenia","SB":"Solomon Islands","SO":"Somalia","ZA":"South Africa","GS":"South Georgia and the South Sandwich Islands","KR":"South Korea","ES":"Spain (España)","LK":"Sri Lanka","SD":"Sudan","SR":"Suriname","SJ":"Svalbard and Jan Mayen","SZ":"Swaziland","SE":"Sweden","CH":"Switzerland","SY":"Syria","ST":"São Tomé and Príncipe","TW":"Taiwan","TJ":"Tajikistan","TZ":"Tanzania","TH":"Thailand","TL":"Timor-Leste","TG":"Togo","TK":"Tokelau","TO":"Tonga","TT":"Trinidad and Tobago","TN":"Tunisia","TR":"Turkey","TM":"Turkmenistan","TC":"Turks and Caicos Islands","TV":"Tuvalu","UM":"U.S. Minor Outlying Islands","PU":"U.S. Miscellaneous Pacific Islands","VI":"U.S. Virgin Islands","UG":"Uganda","UA":"Ukraine","SU":"Union of Soviet Socialist Republics","AE":"United Arab Emirates","GB":"United Kingdom","US":"United States","ZZ":"Unknown or Invalid Region","UY":"Uruguay","UZ":"Uzbekistan","VU":"Vanuatu","VA":"Vatican City","VE":"Venezuela","VN":"Vietnam","WK":"Wake Island","WF":"Wallis and Futuna","EH":"Western Sahara","YE":"Yemen","ZM":"Zambia","ZW":"Zimbabwe"};
+    };
+
+    // World Countries select list
+    var fillWorldCountriesSelect = function(){
+      var spain_code = 'ES';
+      var input = $('[name="fullstripe_address_country"]');
+      if (input.length){
+        var countries = _getWorldCountries();
+        for (var i in countries){
+          var country_name = countries[i];
+          var selected = '';
+          if (i == spain_code) selected = ' selected="selected"';
+          input.append('<option value="'+i+'"'+selected+'>'+country_name+'</option>');
+        }
+
+        input.change(function (){
+          var country_code = $(this).val();
+          if (country_code == spain_code){
+            fillRegionSelectWithSpanishProvinces();
+          }else{
+            changeRegionSelectToInput();
+          }
+        });
+
+        fillRegionSelectWithSpanishProvinces();
+      }
+    };
+    fillWorldCountriesSelect();
 
     // document type selector
     var doctype_selector = $('#doctype-selector');
     if (doctype_selector.length){
       var doctype_values = $('#doctype-values');
       doctype_values.find('> div').hide();
-      doctype_selector.find('input[name="fullstripe_doctype"]').change(function(e){
+      doctype_selector.find('[name="fullstripe_doctype"]').change(function(e){
         var value = $(this).val();
         doctype_values.find('> div').hide();
         doctype_values.find('[data-type="'+value+'"]').fadeIn(300);
