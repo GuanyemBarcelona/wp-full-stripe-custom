31cef458f11c07e4fad7749770d86967701b2d97
Felip Ladron <fladron@omitsis.com>
2014-11-21 15:35:35 +0100

    Changed dni verification to accept foreigner dnis

--- include/wp-full-stripe-customer.php
+++ include/wp-full-stripe-customer.php
@@ -24,14 +24,23 @@ class MM_WPFS_Customer
         add_action('wp_ajax_nopriv_fullstripe_checkout_form_charge', array($this, 'fullstripe_checkout_charge'));
     }
 
-    private static function is_valid_dni_nie($string) {
-      if (strlen($string) != 9 ||
-          preg_match('/^[XYZ]?([0-9]{7,8})([A-Z])$/i', $string, $matches) !== 1) {
-          return false;
+    private static function is_valid_dni_nie($dni) {
+      if(strlen($dni) < 9) {
+        return false;
       }
-      $map = 'TRWAGMYFPDXBNJZSQVHLCKE';
-      list(, $number, $letter) = $matches;
-      return strtoupper($letter) === $map[((int) $number) % 23];
+      $dni = strtoupper($dni);
+      $letra = substr($dni, -1, 1);
+      $numero = substr($dni, 0, 8);
+      // Si es un NIE hay que cambiar la primera letra por 0, 1 ó 2 dependiendo de si es X, Y o Z.
+      $numero = str_replace(array('X', 'Y', 'Z'), array(0, 1, 2), $numero); 
+    
+      $modulo = $numero % 23;
+      $letras_validas = "TRWAGMYFPDXBNJZSQVHLCKE";
+      $letra_correcta = substr($letras_validas, $modulo, 1);
+      if($letra_correcta != $letra) {
+        return false;
+      }
+      return true;
     }
 
     private static function is_valid_ccc($ccc) {
